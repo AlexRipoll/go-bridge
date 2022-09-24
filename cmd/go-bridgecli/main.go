4e90ed3ff6a970df37ad02a411f31fc3b06a2f6c
Alex Ripoll <alexripollpiera@gmail.com>
2022-09-24 13:07:25 +0200

    adding function call cli commands

--- cmd/go-bridgecli/main.go
+++ cmd/go-bridgecli/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"github.com/AlexRipoll/go-bridge/blockchain/config"
 	"github.com/AlexRipoll/go-bridge/blockchain/core"
 	"github.com/AlexRipoll/go-bridge/blockchain/core/evm"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/pkg/errors"
 	"log"
+	"math/big"
+	"strconv"
 )
 
 func main() {
+	fn := flag.Bool("fn", false, "executes a function")
 	deploy := flag.Bool("deploy", false, "deploy contract abis")
 
 	flag.Parse()
@@ -23,26 +27,27 @@ func main() {
 
 	var custodian evm.Custodian
 	bridgers := make(map[string]evm.Bridger)
-	for _, n := range config.Networks {
-		conn, err := ethclient.Dial(n.Url)
+	for name, network := range config.Networks {
+		conn, err := ethclient.Dial(network.Url)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		switch n.Type {
-		case "custodian":
-			custodian, err = evm.NewCustodian(conn, n.Contracts["vault"], config, n.Name)
-			if err != nil {
-				log.Fatal(err)
+		for contract, address := range network.Contracts {
+			switch contract {
+			case "vault":
+				custodian, err = evm.NewCustodian(conn, address, config, name)
+				if err != nil {
+					log.Fatal(err)
+				}
+			case "bridge":
+				bridger, err := evm.NewBridger(conn, address, config, name)
+				if err != nil {
+					log.Fatal(err)
+				}
+				bridgers[name] = bridger
+			default:
+				log.Fatal("unknown network type")
 			}
-		case "bridger":
-			bridger, err := evm.NewBridger(conn, n.Contracts["bridge"], config, n.Name)
-			if err != nil {
-				log.Fatal(err)
-			}
-			bridgers[n.Name] = bridger
-		default:
-			log.Fatal("unknown network type")
 		}
 
 	}
@@ -50,6 +55,58 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if *fn {
+		switch flag.Arg(0) {
+		case "mint":
+			walletAddr := flag.Arg(1)
+
+			networkName := config.MainNetwork
+			network := config.Networks[networkName]
+			conn, err := ethclient.Dial(network.Url)
+			if err != nil {
+				log.Fatal(err)
+			}
+			minter, err := evm.NewBridger(conn, network.Contracts["bridge"], config, networkName)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bridgers[networkName] = minter
+
+			tx, err := minter.Mint(context.Background(), networkName, walletAddr, big.NewInt(6) )
+			if err != nil {
+				log.Fatal(" error minting token: ", err)
+			}
+			fmt.Println(fmt.Sprintf("%#v", tx))
+		case "transfer":
+			destinationBlockchain := flag.Arg(1)
+			originBlockchain := flag.Arg(2)
+			walletAddr := flag.Arg(3)
+			tokenIdArg := flag.Arg(4)
+			tokenId, err := strconv.Atoi(tokenIdArg)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err =bridge.TransferNFT(context.Background(), destinationBlockchain, originBlockchain, walletAddr, big.NewInt(int64(tokenId)))
+			if err != nil {
+				log.Fatal(" error transferring token: ", err)
+			}
+			fmt.Println(">> token transfer success")
+		case "wallet":
+			walletAddr := flag.Arg(0)
+			blockchain := flag.Arg(1)
+
+			tokens, err := bridge.WalletTokens(walletAddr, blockchain)
+			if err != nil {
+				log.Fatalf("error fetching tokens: %v", err)
+			}
+			fmt.Println("Token Ids: ", tokens)
+		default:
+			log.Fatalf("unknow method: %s", flag.Arg(0))
+		}
+	}
+
 	if *deploy {
 		if err = bridge.Deploy(context.Background()); err != nil {
 			log.Fatal(errors.Wrap(err, "error deploying contract"))
