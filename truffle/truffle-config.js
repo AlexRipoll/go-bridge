42b42e15fae11c6090afb46a3901bbd7f836ad70
Alex Ripoll <alexripollpiera@gmail.com>
2022-10-27 19:58:43 +0200

    updating truffle-config

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -6,27 +6,29 @@ const privateKey = process.env["privateKey"];
 
 module.exports = {
     networks: {
-        dev: {
-            host: "127.0.0.1",
-            port: 9545,
-            network_id: "*",
-            websockets: true
-        },
+        // dev: {
+        //     host: "127.0.0.1",
+        //     port: 9545,
+        //     network_id: "*",
+        //     websockets: true
+        // },
         goerli: {
             provider: function() {
-                return new HDWalletProvider(mnemonic, "https://goerli.infura.io/v3/72902afeffe44eeba2ed93d798f87ebd");
+                return new HDWalletProvider(privateKey, "https://goerli.infura.io/v3/72902afeffe44eeba2ed93d798f87ebd");
             },
             network_id: '5',
+            gas: 4465030,
+            gasPrice: 63363054162,
         },
         mumbai: {
             provider: function() {
-                return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/YOUR-PROJECT-ID");
+                return new HDWalletProvider(privateKey, "https://matic-mumbai.chainstacklabs.com");
             },
             network_id: '80001',
         },
-        bscTestnet: {
+        bsct: {
             provider: function() {
-                return new HDWalletProvider(mnemonic, "https://data-seed-prebsc-1-s1.binance.org:8545");
+                return new HDWalletProvider(privateKey, "https://data-seed-prebsc-1-s1.binance.org:8545/");
             },
             network_id: '97',
         },
