99b3cd85b07c714873d5d16b7ecf0c1a6c2799f0
Alex Ripoll <alexripollpiera@gmail.com>
2022-10-27 20:09:12 +0200

    moving .env to truffle directory

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: '../.env' });
+require("dotenv").config({ path: '.env' });
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const mnemonic = process.env["mnemonic"];
